30b1b63c6e8e4219d5ebd97e05ad36b022400883
Ahuja, Ritika <RitikaAhuja@landolakes.com>
2020-10-28 11:32:49 +0530

    PS: Algorithms & DS - Part 1

---
+++ DataStructures/LinkedListsDemo/DoublyLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedListsDemo
+{
+    public class DoublyLinkedList<T> : ICollection<T>
+    {
+        #region ICollection
+        public int Count => throw new System.NotImplementedException();
+
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        public DoublyLinkedListNode<T> Head { get; private set; }
+        public DoublyLinkedListNode<T> Tail { get; private set; }
+
+        public void AddHead(T value)
+        {
+            DoublyLinkedListNode<T> newHead = new DoublyLinkedListNode<T>(value);
+
+            if (Head != null)
+            {
+                Head.Previous = newHead;
+                newHead.Next = Head;
+            }
+
+            Head = newHead;
+
+            if (Tail == null)
+                Tail = Head;
+
+            Count++;
+        }
+        //public bool GetHead(out T value) { }
+        //public bool GetTail(out T value) { }
+    }
+}
---
+++ DataStructures/LinkedListsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<int> ints = new DoublyLinkedList<int>();
+            for(int i = 1; i <= 5; i++)
+            {
+                ints.AddHead(i);
+            }
+        }
+    }
+}
